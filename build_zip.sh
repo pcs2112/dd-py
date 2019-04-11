a167d8a0896ed9543d23bc54505ce6ae560faa4d
Vicente Chavez <vchavez92780@gmail.com>
2019-04-11 14:55:21 -0400

    Added shell script to create the app zip file

---
+++ build_zip.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+_cwd="$PWD"
+_targetRoot="$HOME/Downloads"
+_rootDirName="dd-py"
+
+rm -rf "$_targetRoot/$_rootDirName"
+rm -rf "$_targetRoot/$_rootDirName.zip"
+mkdir "$_targetRoot/$_rootDirName"
+mkdir "$_targetRoot/$_rootDirName/in"
+cp -r "$_cwd/src" "$_targetRoot/$_rootDirName/src"
+cp -v "$_cwd/.env.example" "$_targetRoot/$_rootDirName/.env"
+cp "$_cwd/app.py" "$_targetRoot/$_rootDirName/app.py"
+cp "$_cwd/requirements.txt" "$_targetRoot/$_rootDirName/requirements.txt"
+find "$_targetRoot/$_rootDirName" | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
+cd "$_targetRoot/${_rootDirName}"
+zip -r "$_targetRoot/$_rootDirName.zip" .
