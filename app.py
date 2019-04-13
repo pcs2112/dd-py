088a43d4c07f90595ed093b46a631dc8fc44681d
Vicente Chavez <vchavez92780@gmail.com>
2019-04-12 23:39:20 -0400

    Fixed bug when saving the product images

--- app.py
+++ app.py
@@ -14,21 +14,22 @@ def main(args):
 	try:
 		module = importlib.import_module('src.commands')
 		cmd = getattr(module, args[1])
-	except KeyError as e:
+	except AttributeError:
 		print(f"\"{args[1]}\" is an invalid command.")
 		exit()
-	
+		
 	try:
 		if len(args) > 2:
 			cmd(*args[2:len(args)])
 		else:
 			cmd()
-		
+		close_db_connections()
+	except KeyboardInterrupt as e:
 		close_db_connections()
 	except Exception as e:
 		close_db_connections()
 		raise e
-
+	
 
 if __name__ == '__main__':
 	main(sys.argv)
--- src/commands/build_product_images.py
+++ src/commands/build_product_images.py
@@ -9,7 +9,7 @@ app_config = get_config()
 profile_db = get_db_connection('profile')
 core_db = get_db_connection('core')
 
-out_dir = app_config['OUT_DIR']
+out_dir = f"{app_config['OUT_DIR']}/products"
 in_dir = app_config['IN_DIR']
 cdn_base_path = app_config['CDN_BASE_PATH']
 
@@ -48,7 +48,8 @@ def get_product_colors(product_id):
         "   `product` AS `p` "
         "INNER JOIN `product-colour` AS `pc` ON `p`.`productColourID` = `pc`.`colourID` "
         "WHERE "
-        "   `productParentID` = %s"
+        "   `productParentID` = %s "
+        "GROUP BY `pc`.`colourID`"
     )
 
     cursor = profile_db.cursor()
@@ -79,15 +80,20 @@ def get_product_color_images(product_id):
     
     
 def build_product_images(product_ids=''):
+    try:
+        shutil.rmtree(out_dir)
+        print(f'Emptied {out_dir}')
+    except FileNotFoundError:
+        pass
+
+    os.mkdir(out_dir)
+    
     masters = get_all_master_products(product_ids)
     
     for product in masters:
         product_id = product[0]
         product_code = product[1]
         product_name = product[3]
-        
-        out_dir_product_code = product_code.replace('/', '-')
-        out_file_product_code = product_code.lower().replace('/', '')
         
         print(f'Processing product id={product_id} code={product_code} name={product_name}')
        
@@ -102,37 +108,57 @@ def build_product_images(product_ids=''):
                 'color_name': color[1],
                 'images': []
             }
-        
+            
         for color_image in colors_images:
             color_id = color_image[1]
             if color_id in colors_map:
                 colors_map[color_id]['images'].append(color_image)
-        
-        try:
-            out_product_dir = os.path.join(out_dir, out_dir_product_code)
-            
-            try:
-                shutil.rmtree(out_product_dir)
-            except FileNotFoundError:
-                pass
-                
-            os.mkdir(out_product_dir)
 
+        # Product directory name
+        out_product_dir = os.path.join(out_dir, product_code.replace('/', '-'))
+
+        # Remove the product directory
+        try:
+            shutil.rmtree(out_product_dir)
+        except FileNotFoundError:
+            pass
+        
+        product_dir_created = False
+        try:
+            # Create the product directory
+            os.mkdir(out_product_dir)
+            product_dir_created = True
+        except OSError:
+            print("Creation of the directory %s failed" % out_product_dir)
+            pass
+
+        if product_dir_created:
+            # Iterate over each product color
             for color_id, color in colors_map.items():
+                #  Iterate over each product color image
                 for color_image in color['images']:
                     color_image_id = color_image[0]
-                    
+            
                     # Iterate over the image path fields in the product-image data
                     for i in range(2, 4):
-                        existing_path = color_image[i]
-                        if existing_path:
-                            existing_path = os.path.join(in_dir, existing_path.replace(cdn_base_path, '').lstrip('/'))
-                            exists = os.path.isfile(existing_path)
-                            if exists:
-                                filename = os.path.basename(existing_path).replace(f'{out_file_product_code}_', '')
+                        if color_image[i]:
+                            # Get the path for the color image
+                            existing_path = os.path.join(in_dir, color_image[i].replace(cdn_base_path, '').lstrip('/'))
+                            if os.path.isfile(existing_path):
+                                # Get the existing file name from the path
+                                existing_filename = os.path.basename(existing_path)
+                        
+                                # Remove the product code reference from the existing file name
+                                existing_filename = existing_filename.replace(
+                                    f"{product_code.lower().replace('/', '')}_", ''
+                                )
+                                
+                                # Create the new file name
                                 new_filename = re.sub('[^0-9a-zA-Z]+', '-', color['color_name'].lower())
-                                new_filename = os.path.join(out_product_dir, f'{new_filename}_{color_id}_{color_image_id}_{filename}')
+                                new_filename = os.path.join(
+                                    out_product_dir,
+                                    f'{new_filename}_{color_id}_{color_image_id}_{existing_filename}'
+                                )
+                        
+                                # Copy the existing file into the new file
                                 shutil.copyfile(existing_path, new_filename)
-        except OSError as e:
-            print("Creation of the directory %s failed" % out_product_dir)
-            raise e
