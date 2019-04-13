d064fb3333c27df838c7a33319271a5fe256170c
Vicente Chavez <vchavez92780@gmail.com>
2019-04-13 10:43:03 -0400

    Improved exception handling and renamed db helper

--- app.py
+++ app.py
@@ -1,34 +1,41 @@
 import sys
 import importlib
-from src.utils import close_db_connections
+from src.db import close_db_connections
 
 
 arg_count = len(sys.argv)
 
 
+def _exit(_exit=True):
+	close_db_connections()
+	if _exit:
+		exit()
+
+
 def main(args):
 	if len(args) == 1:
 		print("Please enter a command.")
-		exit()
+		_exit()
 
 	try:
 		module = importlib.import_module('src.commands')
 		cmd = getattr(module, args[1])
 	except AttributeError:
 		print(f"\"{args[1]}\" is an invalid command.")
-		exit()
+		_exit()
 		
 	try:
 		if len(args) > 2:
 			cmd(*args[2:len(args)])
 		else:
 			cmd()
-		close_db_connections()
 	except KeyboardInterrupt:
-		close_db_connections()
+		pass
 	except Exception as e:
-		close_db_connections()
+		_exit(False)
 		raise e
+		
+	_exit()
 	
 
 if __name__ == '__main__':
